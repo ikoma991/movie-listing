e82025a9227f61e963f50dea1e64603c6e646bcd
Muhammad Usama <liontiger49@yahoo.com>
2025-02-18 16:49:45 +0500

    quotes issue eslint fix

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -26,7 +26,7 @@ export default async function Search( {
                 </header>
                 <section className="space-y-6">
                     <h2 className="text-2xl font-semibold text-gray-200">
-                    Search Results for "{query}"
+                    Search Results for &quot;{query}&quot;
                     </h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {searchResults.map((movie: Movie) => (
